c105bf3fc1709bf9691cc557df7c080857214e59
Juri Berlanda <juriberlanda@hotmail.com>
2015-11-08 21:53:54 +0100

    [Motion] Implemented motion sensor and alarm LED.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- BII_Pr2-Arduino.ino
+++ BII_Pr2-Arduino.ino
@@ -10,6 +10,12 @@
 // Green
 #define LIGHTSENSOR_PIN 3
 
+// Brown
+#define MOTIONSENSOR_PIN 4
+
+// UNO Onboard LED
+#define LED_PIN 13
+
 // Serial speed
 #define SERIAL_BAUDRATE 115200
 
@@ -31,6 +37,7 @@ struct Bme280Data {
 // Global variables
 int buttonState = 0;
 int light = 0;
+int motionDetected = 0;
 struct Bme280Data bme280Data;
 
 
@@ -48,18 +55,29 @@ void setup() {
 
   // Light sensor pin needs no init
 
+  // Init MotionSensor pin
+  pinMode(MOTIONSENSOR_PIN, INPUT);
+
+  // Init LED pin
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
   // Init BME280 driver
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
     while (1);
   };
+
+  // Flush serial buffer
+  while (Serial.available() && Serial.read() != '\n');
 }
 
 void loop() {
-  // Read data
+  // Read data from sensors
   readButtonState();
   readLightsensor();
   readHumPressTemp();
+  readMotionSensor();
 
   // Send PushButton state
   Serial.print("b\t");Serial.println(buttonState);
@@ -67,10 +85,16 @@ void loop() {
   // Send Lightsensor data
   Serial.print("l\t");Serial.println(light);
 
+  // Send MotionDetector data
+  Serial.print("m\t");Serial.println(motionDetected);
+
   // Send BME280 data
   Serial.print("h\t");Serial.println(bme280Data.humidity);
   Serial.print("p\t");Serial.println(bme280Data.pressure);
   Serial.print("t\t");Serial.println(bme280Data.temperature);
+
+  // Read LED value
+  readSerialCommand();
 
   // Since we are running at 24 FPS, Frametime is ~40msec
   delay(20);
@@ -85,6 +109,10 @@ void readLightsensor() {
   light = analogRead(LIGHTSENSOR_PIN);
 }
 
+void readMotionSensor() {
+  motionDetected = digitalRead(MOTIONSENSOR_PIN);
+}
+
 void readHumPressTemp() {
   bme280Data.humidity = bme.readHumidity();
   // Read and convert to hPa
@@ -92,5 +120,27 @@ void readHumPressTemp() {
   bme280Data.temperature = bme.readTemperature();
 }
 
+void readSerialCommand() {
+  char command = 0;
+  char separator = 0;
+  char value = 0;
 
+  if (Serial.available()) {
+    command = Serial.read();
+    separator = Serial.read();
+    value = Serial.read();
+
+    if (separator == '\t')
+      switch(command) {
+        case 'l':
+          if (value == '1')
+            digitalWrite(LED_PIN, HIGH);
+          else
+            digitalWrite(LED_PIN, LOW);
+      }
+
+    // Flush serial buffer
+    while (Serial.available() && Serial.read() != '\n');
+  }
+}
 
